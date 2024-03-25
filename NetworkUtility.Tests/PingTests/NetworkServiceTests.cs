c396a38a886cc2bf7cfe6264b36920f1f68ef948
yigitek <yigitek@gmail.com>
2024-03-25 14:18:43 +0100

    Added first

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange - vars, classes, mocks
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.SendPing();
+
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            //SearchDNS();
+            //BuildPacket();
+            return "Success: Ping Sent!";
+        }
+    }
+}
